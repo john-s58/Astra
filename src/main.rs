563873fdc27e86c67b784b7c29af0f7311cddfb1
Deus58 <jonathansarig58@gmail.com>
2023-02-01 14:32:04 +0200

    Changed network to use trait object Layer

--- src/main.rs
+++ src/main.rs
@@ -3,164 +3,148 @@ use rand::Rng;
 use rand_distr::StandardNormal;
 use serde::{Deserialize, Serialize};
 
-#[derive(PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
-pub enum ActivationFunction {
-    InputLayer,
-    ReLU,
-    Sigmoid,
-    Tanh,
+pub trait Layer {
+    fn feed_forward(&mut self, inputs: &Vec<f32>) -> Vec<f32>;
+
+    fn back_propagation(&mut self, error: DMatrix<f32>, learning_rate: f32) -> DMatrix<f32>; 
+
+    fn get_weights(&self) -> DMatrix<f32>;
+
+    fn get_biases(&self) -> DMatrix<f32>;
 }
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Net {
-    pub config: Vec<usize>,
-    pub weights: Vec<DMatrix<f32>>,
-    pub biases: Vec<Vec<f32>>,
-    pub activation: Vec<ActivationFunction>,
-    pub mutation_rate: Option<f32>,
+
+#[derive(Clone, Debug)]
+pub struct LayerDense {
+    size: usize,
+    weights: DMatrix<f32>,
+    biases: DMatrix<f32>,
+    activation: fn(f32) -> f32,
+    activation_derivative: fn(f32) -> f32,
+    input: Option<DMatrix<f32>>,
+    output: Option<DMatrix<f32>>,
 }
-// CONSIDER BIAS FOR LAYER AND NOT ONLY FOR EACH NEURON
-impl Net {
-    pub fn new() -> Self{
-        Self {
-            config: Vec::<usize>::new(),
-            weights: Vec::<DMatrix<f32>>::new(),
-            biases: Vec::<Vec<f32>>::new(),
-            activation: Vec::<ActivationFunction>::new(),
-            mutation_rate: None,
-        }
-    }
-
-    pub fn add_layer(mut self, layer_size: usize, layer_activation: ActivationFunction) -> Self{
-        if self.config.len() == 0 {
-            match layer_activation {
-                ActivationFunction::InputLayer => {
-                    self.config.push(layer_size);
-                    self.activation.push(layer_activation);
-                    return self
-                },
-                _ => panic!("Input Layer Activation Must Be InputLayer")
-            }
-        }
-        else {
-            let mut rng = rand::thread_rng();
-
-            // Hat El Initialization
-            self.weights.push(DMatrix::<f32>::from_distribution(*self.config.last().unwrap(), 
-            layer_size, &StandardNormal, &mut rng)
-            * (2. / *self.config.last().unwrap() as f32).sqrt());
-            self.biases.push(
-                (0..layer_size).map(|_| rng.gen_range(0.0..1.0)).collect()
-            );
-            self.config.push(layer_size);
-            self.activation.push(layer_activation);
-
-            return self
-        }
-    }
-
-    pub fn with_mutation(mut self, mutation_rate: f32) -> Self {
-        self.mutation_rate = Some(mutation_rate);
-        self
-    }
-
-    pub fn feed_forward(&self, inputs: &Vec<f32>) -> Vec<f32> {
-        let mut outputs =  DMatrix::from_vec(inputs.len(), 1, inputs.to_vec());
-
-        for ((w, b), 
-                    activ) in self.weights.iter()
-                                                    .zip(self.biases.iter())
-                                                    .zip(self.activation.iter().skip(1)){
-            outputs = w.transpose() * outputs +
-             DMatrix::from_vec(b.len(), 1, b.to_vec());
-            match activ {
-                ActivationFunction::ReLU => {outputs = outputs.map(|x| if x > 0.0 {x} else {0.0})},
-                ActivationFunction::Sigmoid => {outputs = outputs.map(|x| 1.0 / (1.0 + (-x).exp()))},
-                ActivationFunction::Tanh => {outputs = outputs.map(|x| x.tanh())},
-                _ => ()
-            }
-        }
-        outputs.data.into()
-    }
-
-    pub fn back_propagation(self, inputs: &Vec<f32>, targets: &Vec<f32>) -> Self { 
-        // Consider &mut self as param and change original net
-        self
-    }
-
-    pub fn crossover(a: &Net, b: &Net) -> Self {
-        assert_eq!(a.config, b.config, "The 2 Networks Structure Is Not The Same");
+impl LayerDense {
+    fn new(size: usize, input_size: usize, activation: fn(f32) -> f32, activation_derivative: fn(f32) -> f32) -> Self{
         let mut rng = rand::thread_rng();
         Self {
-            config: a.config.to_owned(),
-            activation: a.activation.to_owned(),
-            mutation_rate: a.mutation_rate,
-            weights: a
-                .weights
-                .iter()
-                .zip(b.weights.iter())
-                .map(|(m1, m2)| {
-                    m1.zip_map(m2, |ele1, ele2| {
-                        if rng.gen_range(0.0..1.0) < 0.5 {
-                            ele1
-                        } else {
-                            ele2
-                        }
-                    })
-                })
-                .collect(),
-            biases: a
-                .clone()
-                .biases
-                .into_iter()
-                .zip(b.clone().biases.into_iter())
-                .map(|(b1, b2)| {
-                    b1.into_iter()
-                        .zip(b2.into_iter())
-                        .map(|(ele1, ele2)| {
-                            if rng.gen_range(0.0..1.0) < 0.5 {
-                                ele1
-                            } else {
-                                ele2
-                            }
-                        })
-                        .collect()
-                })
-                .collect(),
+            size,
+            weights: DMatrix::<f32>::from_distribution(input_size, 
+            size, &StandardNormal, &mut rng).map(|x| if x == 0.0 {x + 0.3} else {x}),
+            biases:  DMatrix::<f32>::from_vec(1, size, vec![0.0f32; size]),
+            activation,
+            activation_derivative,
+            input: None,
+            output: None,
         }
     }
+    fn into_box(self) -> Box<Self> {
+        Box::new(self)
+    }
+}
 
-    pub fn mutate(&mut self) {
-        match self.mutation_rate {
-            Some(m) => {
-                let mut rng = rand::thread_rng();
-                for layer in &mut self.weights {
-                    for weight in layer {
-                        if rng.gen_range(0.0..1.0) < m {
-                            *weight = rng.sample::<f32, StandardNormal>(StandardNormal);
-                        }
-                    }
-                }
-            }
-            None => {}
-        }
+impl Layer for LayerDense{
+    fn feed_forward(&mut self, inputs: &Vec<f32>) -> Vec<f32>{
+        self.input = Some(DMatrix::from_vec(1, inputs.len(), inputs.to_vec()));
+
+        self.output = Some((self.input.as_ref().unwrap() *
+        &self.weights 
+        + self.biases.clone()).map(self.activation));
+
+        self.output.clone().unwrap().data.into()
+
+    }
+
+    fn back_propagation(&mut self, error: DMatrix<f32>, learning_rate: f32) -> DMatrix<f32>{
+        let err = error.component_mul(&self.output.as_ref().unwrap().map(self.activation_derivative));
+
+        // println!("self.weights = {:#?}", self.weights.data.as_vec());
+        // println!("self.biases = {:#?}", self.biases.data.as_vec());
+        // println!("self.output = {:#?}", self.output.clone().unwrap().data.as_vec());
+        // println!("err = {:#?}", err.data.as_vec());
+
+        let delta_weights = self.input.as_ref().unwrap().transpose() * &err;
+        let delta_biases = &err.row_sum() / err.nrows() as f32;
+
+        self.weights -= delta_weights * learning_rate;
+        self.biases -= delta_biases * learning_rate;
+
+
+        // println!("self.weights POST GRADIENT = {:#?}", self.weights.data.as_vec());
+        // println!("self.biases POST GRADIENT = {:#?}", self.biases.data.as_vec());
+
+        err * self.weights.transpose()
+    }
+
+    fn get_weights(&self) -> DMatrix<f32> {
+        self.weights.clone()
+    }
+
+    fn get_biases(&self) -> DMatrix<f32>{
+        self.biases.clone()
     }
 
 }
+pub struct Net{
+    layers: Vec<Box<dyn Layer>>,
+}
 
-fn main(){
-    let net = Net::new()
-        .add_layer(3, ActivationFunction::InputLayer)
-        .add_layer(4, ActivationFunction::ReLU)
-        .add_layer(4, ActivationFunction::ReLU)
-        .add_layer(3, ActivationFunction::Sigmoid)
-        .with_mutation(0.15);
+impl Net{
+    pub fn new() -> Self {
+        Self { layers: Vec::new() }
+    }
 
-    let input: Vec<f32> = vec![1.2, 3.4, 5.6];
+    pub fn add_layer(&mut self, layer: Box<dyn Layer>){
+        self.layers.push(layer);
+    }
 
-    //println!("{:#?}", net);
+    pub fn feed_forward(&mut self, input: &Vec<f32>) -> Vec<f32> {
+        let mut output = input.to_vec();
+        for l in self.layers.iter_mut(){
+            output = l.feed_forward(&output);
+        }
+        output
+    }
 
-    let outs = net.feed_forward(&input);
+    pub fn back_propagation(&mut self, input: &Vec<f32>, target: &Vec<f32>) {
+        let mut output = self.feed_forward(input);
+        let mut error: Vec<f32> = target.iter().zip(output.iter()).map(|(y, x)| x - y).collect();
 
-    println!("{:#?}", outs);
+        let mut error_mat = DMatrix::<f32>::from_vec(1, error.len(), error.clone());
+
+        for l in self.layers.iter_mut().rev() {
+            error_mat = l.back_propagation(error_mat, 0.01);
+        }
+    }
+}
+
+fn main () {
+    let mut l1 = Box::new(LayerDense::new(1, 1, |x| x.max(0.0),
+                                                                 |x| if x > 0.0 {1.0} else {0.0}));
+    // let mut l2 = Box::new(LayerDense::new(1, 3, |x| x.max(0.0),
+    //                                                                 |x| if x > 0.0 {1.0} else {0.0}));
+
+
+    let mut my_net = Net::new();
+
+    my_net.add_layer(l1);
+   //my_net.add_layer(l2);
+
+    let mut inputs: Vec<Vec<f32>> = Vec::new();
+    let mut targets: Vec<Vec<f32>> = Vec::new();
+
+    for i in 1..10 {
+        inputs.push(vec![i as f32]);
+        targets.push(vec![(2 * i) as f32]);
+    }
+
+   let test_before = my_net.feed_forward(&inputs[0]);
+   println!("{:#?}", test_before);
+
+   for (ins, targ) in inputs.iter().zip(targets.iter()){
+        my_net.back_propagation(ins, targ);
+   }
+
+   let test_after = my_net.feed_forward(&inputs[0]);
+   println!("{:#?}", test_after);
 
 }
