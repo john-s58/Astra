d0d36c3c8dbe0f5194f2a56fa5b7972fdd991920
Deus58 <jonathansarig58@gmail.com>
2023-03-19 13:10:12 +0200

    moved mutating net to different files

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,18 @@
-use crate::astra_net::activation::{Activation, LeakyReLU, Softmax};
+use crate::astra_net::activation::{LeakyReLU, Softmax};
 use crate::astra_net::layer::LayerDense;
 use crate::astra_net::Net;
+use crate::mutating::net::MutatingNet;
 
 use crate::tensor::Tensor;
 
 mod astra_net;
+mod mutating;
 mod tensor;
-use ndarray_rand::rand_distr::{Distribution, Normal};
 use rand::Rng;
 
-pub struct MutatingNet {
-    config: Vec<usize>,
-    weights: Vec<Tensor>,
-    biases: Vec<Tensor>,
-    activation: Box<dyn Activation>,
-    mutation_rate: f64,
-}
-
-impl MutatingNet {
-    pub fn from_config(config: Vec<usize>) -> Self {
-        let mut rng = rand::thread_rng();
-        let normal = Normal::new(-1.0, 1.0).unwrap();
-        Self {
-            config: config.clone(),
-            weights: config
-                .clone()
-                .into_iter()
-                .zip(config.clone().into_iter().skip(1))
-                .map(|(prev, cur)| {
-                    Tensor::from_vec(
-                        (0..cur * prev).map(|_| normal.sample(&mut rng)).collect(),
-                        vec![cur, prev],
-                    ) * (2.0 / (prev + cur) as f64).sqrt()
-                })
-                .collect(),
-            biases: config
-                .into_iter()
-                .map(|sz| Tensor::from_element(0.0, vec![sz]))
-                .collect(),
-            activation: Box::new(LeakyReLU::new(0.3)),
-            mutation_rate: 0.15,
-        }
-    }
-
-    pub fn feed_forward(&self, input: &Tensor) -> Tensor {
-        let mut output = input.to_owned().reshape(vec![1, input.len()]).unwrap();
-
-        for (weight, bias) in self
-            .weights
-            .to_owned()
-            .into_iter()
-            .zip(self.biases.to_owned().into_iter())
-        {
-            output =
-                output.dot(&weight).unwrap() + bias.clone().reshape(vec![1, bias.len()]).unwrap();
-
-            output = self.activation.call(output)
-        }
-        output
-    }
-
-    pub fn crossover(mut self, right: &Self) -> Option<Self> {
-        if self.config != right.config.to_owned() {
-            return None;
-        }
-
-        let mut rng = rand::thread_rng();
-
-        self.weights = self
-            .weights
-            .into_iter()
-            .zip(right.weights.to_owned().into_iter())
-            .map(|(weights_self, weights_right)| {
-                Tensor::from_vec(
-                    weights_self
-                        .clone()
-                        .into_iter()
-                        .zip(weights_right.into_iter())
-                        .map(|(w_l, w_r)| {
-                            if rng.gen_range(0.0..1.0) < 0.5 {
-                                w_l
-                            } else {
-                                w_r
-                            }
-                        })
-                        .collect(),
-                    weights_self.shape,
-                )
-            })
-            .collect();
-        self.biases = self
-            .biases
-            .into_iter()
-            .zip(right.biases.to_owned().into_iter())
-            .map(|(weights_self, weights_right)| {
-                Tensor::from_vec(
-                    weights_self
-                        .clone()
-                        .into_iter()
-                        .zip(weights_right.into_iter())
-                        .map(|(w_l, w_r)| {
-                            if rng.gen_range(0.0..1.0) < 0.5 {
-                                w_l
-                            } else {
-                                w_r
-                            }
-                        })
-                        .collect(),
-                    weights_self.shape,
-                )
-            })
-            .collect();
-
-        Some(self)
-    }
-}
-
 fn main() {
-    let config = vec![5, 3, 5];
-    let mt = MutatingNet::from_config(config.clone());
-    let mt2 = MutatingNet::from_config(config);
-
-    let inp = Tensor::from_element(1.0, vec![5]);
-    let ff = mt.feed_forward(&inp);
-
-    let mt_co = mt.crossover(&mt2).unwrap();
-    let ff_co = mt_co.feed_forward(&inp);
-
-    println!("{:#?}", ff);
-    println!("{:#?}", ff_co);
-
     test_astra_net_tensor();
+    test_astra_mutating_mutatingnet();
 }
 
 // use bevy::prelude::*;
@@ -256,4 +138,17 @@ fn generate_2d_cluster_dataset(num_samples: usize) -> Vec<Tensor> {
         data.push(Tensor::from_vec(vec![x1, x2], vec![2]));
     }
     data
+}
+
+fn test_astra_mutating_mutatingnet() {
+    let net = MutatingNet::from_config(vec![3, 5, 3]);
+    let net2 = MutatingNet::from_config(vec![3, 5, 3]);
+
+    let net_combined = net.crossover(&net2).unwrap();
+
+    let input = Tensor::from_element(1.3, vec![3]);
+
+    let res = net_combined.feed_forward(&input);
+
+    println!("mut net feed forward result = {:#?}", res);
 }
