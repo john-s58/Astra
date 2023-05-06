fixed backprop python for last layer

main.py
@@ -30,58 +30,68 @@ def d_mse(y, target):
 class dense:
     def __init__(self, neurons, input_sz):
         self.weight = np.random.rand(neurons, input_sz)
-        self.bias = np.random.rand(neurons)
+        self.bias = np.random.rand(neurons, 1)
     
     def ff(self, x):
         self.input = x
-        self.output = relu(self.weight.dot(x) + self.bias)
-        return self.output
+        self.output = self.weight.dot(x) + self.bias
+        return relu(self.output)
     
     def bp(self, dl_dx):
-        #dout_dz = drelu(self.output)
-        #dz_dw = self.input
+        print("dl_dx shape", dl_dx.shape)
 
-        print("x shape: ", self.input.shape)
-        print("out shape: ", self.output.shape)
-        print("dl_dx_d_z shape: ", drelu(self.output).shape)
-        print("dl_dx shape: ", dl_dx.shape)
-        
-        
+        dout_dz = drelu(self.output)
 
-        gradient = (self
-        .input
-        .reshape((len(self.input), 1))
-        .dot(drelu(self.output)
-        .reshape((len(self.output),1))).transpose()
-        .dot(dl_dx))
-    
-        dl_dx0 = (self
-        .weight.transpose()
-        .dot(drelu(self.output).reshape((len(self.output),1)))
-        .dot(dl_dx))
+        dout_dz = dout_dz.reshape((dout_dz.size, 1))
+
+        print("dout_dz shape", dout_dz.shape)
+
+        dz_dw = self.input
+
+        print("dz_dw shape", dz_dw.shape)
+
+        gradient = dz_dw.T * dout_dz * dl_dx
+
+        print("gradient shape", gradient.shape)
+
+        print("weight shape", self.weight.shape)
 
         self.weight -= gradient * 0.001
-        return dl_dx0
+
+        # dl_dx0 = (self
+        # .weight.transpose()
+        # .dot(drelu(self.output).reshape((1, len(self.output))))
+        # .dot(dl_dx))
+
+        return 1
         
 layers = []
 
 for i in range(1, len(config)):
     layers.append(dense(config[i], config[i-1]))
 
-input = np.array([1, 1, 1])
+ins = np.array([1, 1, 1])
+ins = ins.reshape((len(ins), 1))
 
-res = input
+res = ins
 for l in layers:
     res = l.ff(res)
 print(res)
   
 target = np.array([3])
 
+res = res.flatten()
+
+print(f"res {res.shape}, target {target.shape}")
+
 loss = mse(res, target)
 dl_dout_layer = d_mse(res, target)
+dl_dout_layer = dl_dout_layer.reshape((len(dl_dout_layer), 1))
 
 for l in list(reversed(layers)):
     dl_dout_layer = l.bp(dl_dout_layer)
+    break
+
 
 res = input
 for l in layers: